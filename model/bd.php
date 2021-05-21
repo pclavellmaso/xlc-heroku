2b2cc0085d6735da0aca2436bf4573455a722103
pclavellmaso <pclavellmaso@gmail.com>
2021-05-21 14:40:05 +0200

    bd

--- model/bd.php
+++ model/bd.php
@@ -8,11 +8,15 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
+
+console_log('epepepep');
+
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 $bd->set_charset("utf8");
 
 if (mysqli_connect_errno()) {
+    console_log('irror');
     echo 'Failed to connect!';
     exit();
 }
