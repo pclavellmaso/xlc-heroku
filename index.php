cb1077925194bdeb24c64b96973d21175f8f50aa
pclavellmaso <pclavellmaso@gmail.com>
2021-05-21 14:00:25 +0200

    try

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
 
-echo '<h1>EPEPEP</h1>';
+//include("model/bd.php");
+include("model/debug.php");
+
+session_start();
+
+
+if (isset($_GET['accio'])) {
+    $accio = $_GET['accio'];
+} else if (isset($_POST['accio'])) {
+    $accio = $_POST['accio'];
+} else {
+    $accio = null;
+}
+
+
+switch ($accio) {
+
+
+    default:
+        include('vista/inici.php');
+        break;
+}
 ?>
